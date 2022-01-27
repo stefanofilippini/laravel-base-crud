df7ab1a874adbf46dfffc922db8ae397cbaa4d36
stefanofilippini <filippini.stefano96@gmail.com>
2022-01-27 15:54:20 +0100

    created main layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="container">
+        <h1>DC Comics</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ea quaerat cupiditate nesciunt neque ullam! Omnis placeat mollitia fugit nulla totam eaque, cumque consequatur ratione consectetur fugiat et doloribus eos sed porro in nesciunt reprehenderit cum asperiores. Aspernatur, omnis velit? Quisquam soluta reprehenderit qui cum ut? Adipisci sit velit, quo dicta est magni doloribus dolore cupiditate ad laudantium nemo explicabo, perspiciatis quisquam voluptatem dolor illum esse veritatis corrupti deserunt nisi! Vel aspernatur, repudiandae eum consequatur accusantium quia iste odit. Tempora iure vero possimus placeat error nemo? Iusto beatae ullam laboriosam, ea quae laborum nobis ipsa, totam aspernatur possimus vero nemo?</p>
+    </section>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center mt-5">
+    DC Comics - for enemies of marvel
+</footer>
